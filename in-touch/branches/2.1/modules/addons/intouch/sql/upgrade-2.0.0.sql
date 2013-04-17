6de2d8230cbda0111bfa6cab2ceae610d6f3b728
steven_gohigher <steven_gohigher@41da88ad-0e79-4093-98d2-a30be92a1d2c>
2013-04-17 19:02:41 +0000

    *  Changes to trunk merged into 2.1 branch

--- in-touch/branches/2.1/modules/addons/intouch/sql/upgrade-2.0.0.sql
+++ in-touch/branches/2.1/modules/addons/intouch/sql/upgrade-2.0.0.sql
@@ -2,10 +2,3 @@
 
 INSERT INTO `mod_intouch_settings` (`key`, `value`) VALUES ( 'usewysiwyg', '1' )
 
-
--- command split --
-
-
-UPDATE `tbladdonmodules` SET `value` = '2.0.1' WHERE `module` = 'intouch' AND `setting` = 'version'
-
-  
--- in-touch/branches/2.1/modules/addons/intouch/sql/upgrade-2.0.3.sql
+++
@@ -1,4 +0,0 @@
-
-
-UPDATE `tbladdonmodules` SET `value` = '2.0.4' WHERE `module` = 'intouch' AND `setting` = 'version'
-
